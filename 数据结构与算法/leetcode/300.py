3458f6ca0d57b0f3c5f4cb5a7c32970bc1408821
Hasaki1997 <hasaki1997@foxmail.com>
2018-06-07 07:27:24 +0800

    update 2018-6-7-7-27

---
+++ 数据结构与算法/leetcode/300.py
@@ -0,0 +1,20 @@
+def lengthOfLIS(nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+    tails = [0] * len(nums)
+    size = 0
+    for x in nums:
+        i,j = 0,size
+        while i != j:
+            m = (i+j)//2
+            if tails[m]<x:
+                i = m+1
+            else:
+                j = m
+        tails[i] = x
+        size = max(i+1,size)
+    print(size)
+    return size
+lengthOfLIS([10,9,2,5,3,7,101,18])
---
+++ 机器学习/mooc/test.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+
+a = tf.constant([1.0,2.0])
+b = tf.constant([3.0,4.0])
+
+result = a+b
+with tf.Session() as sess:
+    print(sess.run(result))
+print(result)
